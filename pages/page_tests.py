53b9b4edf3bdf9d4f6794f94ef973b0b94cd8604
Goryaeva Kseniya <goryaevakss@gmail.com>
2025-04-05 19:35:04 +0300

    refactor

--- pages/page_tests.py
+++ pages/page_tests.py
@@ -128,6 +128,6 @@ class SearchBook:
         browser.element('.ant-input').press_enter()
 
     def check_name_book(self,value):
-        browser.all(f'//div/a/p[contains(text(),"{value}")]').should(have.text(value))
+        browser.element(f'//div/a/p[contains(text(),"{value}")]').should(have.text(value))
         #Алмазная колесница
 
